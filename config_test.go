78fd9ffacf1a6a5210151065d0bef9080dca6544
Poyan Gerami <poyan.gerami@eniro.com>
2014-02-27 21:22:19 +0100

    added test for config

--- config_test.go
+++ config_test.go
@@ -4,6 +4,31 @@ import (
 	"testing"
 )
 
+var testResourcesDir string = "test-resources/"
+
 func TestConfig(t *testing.T) {
+	file := testResourcesDir + "config.json"
+
+	c, err := LoadConfig(file)
+
+	if err != nil {
+		t.Fail()
+	}
+
+	if c == nil {
+		t.Error("c can not be nil hear ")
+	}
+
+	user := "user"
+
+	if c.DBUser != user {
+		t.Errorf("expected %v but got %v", user, c.DBUser)
+	}
+
+	passwd := "password"
+
+	if c.DBPasswd != passwd {
+		t.Errorf("expected %v but got %v", passwd, c.DBPasswd)
+	}
 
 }
