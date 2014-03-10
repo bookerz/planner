9c35bd670237ac00de2003f70e1633b8b8f6d9e1
Henrik Johansson <dahankzter@gmail.com>
2014-03-10 10:50:04 +0100

    Moved employee handler to separate file

--- employee.go
+++ employee.go
@@ -2,7 +2,10 @@ package main
 
 import (
 	"database/sql"
+	"encoding/json"
 	"log"
+	"net/http"
+	"strconv"
 )
 
 type Employee struct {
@@ -22,4 +25,44 @@ func (e *Employee) Load(tx *sql.Tx) error {
 	}
 
 	return err
+}
+
+func EmployeeHandler(w http.ResponseWriter, r *http.Request, tx *sql.Tx, vars map[string]string) error {
+
+	id, err := strconv.Atoi(vars["id"])
+
+	if err != nil {
+		log.Printf("[EMPLOYEE]: Unable to format input parameter. error: '%v'", err)
+		http.Error(w, "The id have to be a number", http.StatusBadRequest)
+		return err
+	}
+
+	e := &Employee{
+		Id: id,
+	}
+
+	if err = e.Load(tx); err != nil {
+		switch {
+		case err == sql.ErrNoRows:
+			log.Printf("[EMPLOYEE]: Unable to to find employee with id '%v', error: '%v'", id, err)
+			http.Error(w, "Employee not found", http.StatusNotFound)
+		default:
+			log.Printf("[EMPLOYEE]: Unable to load data from database, error: '%v'", err)
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+		}
+		return err
+	}
+
+	b, err := json.Marshal(e)
+
+	if err != nil {
+		log.Printf("[EMPLOYEE]: Unable to marshal json data, error: '%v'", err)
+		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+		return err
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(b)
+
+	return nil
 }
--- main.go
+++ main.go
@@ -2,13 +2,11 @@ package main
 
 import (
 	"database/sql"
-	"encoding/json"
 	"flag"
 	"fmt"
 	"log"
 	"math/rand"
 	"net/http"
-	"strconv"
 
 	"github.com/julienschmidt/httprouter"
 	_ "github.com/lib/pq"
@@ -55,44 +53,4 @@ func main() {
 	http.Handle("/", r)
 
 	log.Fatal(http.ListenAndServe(":8080", nil))
-}
-
-func EmployeeHandler(w http.ResponseWriter, r *http.Request, tx *sql.Tx, vars map[string]string) error {
-
-	id, err := strconv.Atoi(vars["id"])
-
-	if err != nil {
-		log.Printf("[EMPLOYEE]: Unable to format input parameter. error: '%v'", err)
-		http.Error(w, "The id have to be a number", http.StatusBadRequest)
-		return err
-	}
-
-	e := &Employee{
-		Id: id,
-	}
-
-	if err = e.Load(tx); err != nil {
-		switch {
-		case err == sql.ErrNoRows:
-			log.Printf("[EMPLOYEE]: Unable to to find employee with id '%v', error: '%v'", id, err)
-			http.Error(w, "Employee not found", http.StatusNotFound)
-		default:
-			log.Printf("[EMPLOYEE]: Unable to load data from database, error: '%v'", err)
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-		}
-		return err
-	}
-
-	b, err := json.Marshal(e)
-
-	if err != nil {
-		log.Printf("[EMPLOYEE]: Unable to marshal json data, error: '%v'", err)
-		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
-		return err
-	}
-
-	w.Header().Set("Content-Type", "application/json")
-	w.Write(b)
-
-	return nil
 }
