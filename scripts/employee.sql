4e720cd5a758f3ea2a1f296a36342f838f61ec1f
Poyan Gerami <poyan.gerami@eniro.com>
2014-04-10 19:10:42 +0200

    Added new create employee script.

--- scripts/employee.sql
+++ scripts/employee.sql
@@ -1,6 +1,6 @@
 CREATE SEQUENCE employee_seq start 100;
 
-CREATE TABLE employee IF NOT EXISTS (
+CREATE TABLE employee (
 	id bigint default nextval('employee_seq'),
 	first_name varchar(100),
 	last_name varchar(100),
