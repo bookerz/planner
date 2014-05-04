92f5561163904437ffd32e048f34d1ceae7314d4
Henrik Johansson <dahankzter@gmail.com>
2014-05-04 11:49:08 +0200

    Refactor rerun to allow for Mac integration

--- rerun.sh
+++ rerun.sh
@@ -1,26 +1,32 @@
-#!/bin/sh
+#!/bin/bash
 
 # get the current path
 CURPATH=`pwd`
 
 mkdir /tmp/planner_logs
 
-inotifywait -m --timefmt '%d/%m/%y %H:%M' --format '%T %w %f' \
- -e moved_to . | while read date time dir file; do
+OS=`uname`
 
-	if [[ $file == *.go ]]; then
-		echo $file
-		FILECHANGE=${dir}${file}
-       	# convert absolute path to relative
-       	FILECHANGEREL=`echo "$FILECHANGE" | sed 's_'$CURPATH'/__'`
+echo $OS
 
-       	make
-       	make test
-       	killall planner
-       	./target/planner -v=2 -log_dir="/tmp/planner_logs" -config=./scripts/planner.cfg &
+function act_on_event {
+       file=$1
+       if [[ $file == *.go ]]; then
+              echo $file
+              FILECHANGE=${dir}${file}
+              # convert absolute path to relative
+              FILECHANGEREL=`echo "$FILECHANGE" | sed 's_'$CURPATH'/__'`
+              make
+              make test
+              killall planner
+              ./target/planner -v=2 -log_dir="/tmp/planner_logs" -config=./scripts/planner.cfg &
               echo ""
               echo "============================================================================="
               echo ""
-	fi
-       
+       fi
+}
+
+inotifywait -m --timefmt '%d/%m/%y %H:%M' --format '%T %w %f' \
+ -e moved_to . | while read date time dir file; do
+       act_on_event $file        
 done
